c7c7edce0db110b9c40c2e4fabc921cc808fa0a1
Chriswfxg <chris.john@whatfix.com>
2024-10-15 18:51:39 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ let rules = {
 
 // Send a message and get a response from the bot
 function sendMessage() {
-    const userInput = document.getElementById("user-input");
+    const userInput = document.getElementById("user-input").value;
     const messageBox = document.getElementById("message-box");
 
     // Show user input in chat (aligned to the left)
